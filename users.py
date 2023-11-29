3628747be1e5cdf5deb1dca4700e83d70603bd66
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-11-29 13:37:58 -0600

    Usando Path parameters y Query parameters

--- users.py
+++ users.py
@@ -9,16 +9,16 @@ app = FastAPI()
 # Entidad user
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int 
 
 # Simulando que es una DB.
-usersFakeDB = [ User(name="Jean", surname="Garcia", url="https://jeangacia.com", age=26),
-                User(name="James", surname="Garcia", url="https://jamesgacia.com", age=16),
-                User(name="Alfredo", surname="Garcia", url="https://alfredogacia.com", age=58),]
-
+usersFakeDB = [ User(id= 1,name="Jean", surname="Garcia", url="https://jeangacia.com", age=26),
+                User(id= 2,name="James", surname="Garcia", url="https://jamesgacia.com", age=16),
+                User(id= 3,name="Alfredo", surname="Garcia", url="https://alfredogacia.com", age=58),]
 
 @app.get("/usersjson")
 async def usersJson():
@@ -34,3 +34,20 @@ async def usersClass():
 @app.get("/users")
 async def users():
     return usersFakeDB
+
+# Usando Path parameters.
+@app.get("/user/{id}")
+async def userById(id: int):
+    return searchUser(id)
+    
+# Usando Query parameters.
+@app.get("/userquery/")
+async def userQuery(id: int):
+    return searchUser(id)
+    
+def searchUser(id: int):
+    users = filter(lambda user: user.id == id, usersFakeDB)
+    try:
+        return list(users)[0] 
+    except:
+        return {"error": "Not Found"}
