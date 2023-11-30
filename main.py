b7efd2ccf53d24c2347545febc745c9f48bb72e2
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-11-30 12:41:58 -0600

    Exposing static resources

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
  
 app = FastAPI()
 
@@ -16,6 +17,15 @@ app = FastAPI()
 app.include_router(products.router)
 
 app.include_router(users.router)
+
+# * Exponiendo recursos ESTATICOS. Tales como imagenes, PDF's, etc.
+# El primer parametro, llamado path, corresponde al path raiz que se ocupara para acceder al recurso
+# El segundo parametro, llamado app, es donde se encuentra el recurso a exponer. En este caso se expone todo el directorio "static"
+# por ello en la ruta debemos agregar el subdirectorio "/images" para acceder a la imagen "think=twice.png"
+# El tercer parametro, llamado name, es el nombre de lo que se expondra
+
+# http://127.0.0.1:8000/static/images/think_twice.png
+app.mount("/static", StaticFiles(directory="static"), "static")
 
 # URL local: http://127.0.0.1:8000
 @app.get("/")
