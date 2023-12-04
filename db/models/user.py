a6561ef55252e305e4052c65a3a1d21263664e78
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-03 20:30:57 -0600

    Change properties and seting id to None

--- db/models/user.py
+++ db/models/user.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 # Entidad User (Modelo)
 
 class User(BaseModel):
-    id: int
+    # ! IMPORTANTE: Tuve que igualarlo a None por defecto 
+    # ! porque si no me pedia en el requeste el campo "id"
+    id: str | None = None 
     username: str
     email: str
