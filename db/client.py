4a310492014bf5eafba8007cd9b0034a4e6957b3
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2023-12-07 14:53:55 -0600

    Update information of comments

--- db/client.py
+++ db/client.py
@@ -1,3 +1,8 @@
+
+# Levantar server de MongoDB en local: 
+#       ./mongod --dbpath "/home/jean/Documents/backend/MongoDB/data/"
+#   Nota: Se debe posicionar en el path: /home/jean/Documents/backend/MongoDB/bin
+
 from pymongo import MongoClient
 
 dbClient = MongoClient()
--- routers/users_db.py
+++ routers/users_db.py
@@ -1,8 +1,6 @@
 
 # Users API con MongoDB
-# Levantar server de MongoDB en local: 
-#       ./mongod --dbpath "/home/jean/Documents/backend/MongoDB/data/"
-#   Nota: Se debe posicionar en el path: /home/jean/Documents/backend/MongoDB/bin
+
 
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
